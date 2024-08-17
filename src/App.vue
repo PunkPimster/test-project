aeaea4930efb05c00f34906309f75ace709d854b
PunkPimster <44343595+PunkPimster@users.noreply.github.com>
2024-08-17 13:30:18 +0200

    SRC upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CookieView/>
+</template>
+
+<script>
+import CookieView from './views/CookieView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CookieView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
